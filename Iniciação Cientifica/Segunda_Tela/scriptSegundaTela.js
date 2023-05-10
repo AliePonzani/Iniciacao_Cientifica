60aba0ec0be54efa71aa0bf6e8efe86f6f468fe8
AliePonzani <115513954+AliePonzani@users.noreply.github.com>
2023-05-10 15:47:27 +0000

    atualizado

--- Iniciação Cientifica/Segunda_Tela/scriptSegundaTela.js
+++ Iniciação Cientifica/Segunda_Tela/scriptSegundaTela.js
@@ -25,25 +25,111 @@ class Celula {
 }
 var teste = 1;
 
-function Chamar(){
-    let resultado = document.getElementById('teste');
-    resultado.innerHTML = "teste";
+function Calculo_D(){
+    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    let D1 = document.getElementById('D1');
+    let D2 = document.getElementById('D2');
+    let D3 = document.getElementById('D3');
+    let D = document.getElementById('D');
+
+    D1.innerHTML = `D= ((${valor.aX} * ${valor.bY} * ${valor.cZ}) + (${valor.aY} * ${valor.bZ} * ${valor.cX}) + (${valor.aZ} * ${valor.bX} * ${valor.cY})) - ((${valor.aY} * ${valor.bX} * ${valor.cZ}) + (${valor.aX} * ${valor.bZ}* ${valor.cY}) + (${valor.aZ} * ${valor.bY} * ${valor.cX}))`; 
+    D2.innerHTML = `D= (${valor.aX*valor.bY*valor.cZ} + ${valor.aY*valor.bZ*valor.cX} + ${valor.aZ*valor.bX*valor.cY}) - (${valor.aY*valor.bX*valor.cZ} + ${valor.aX*valor.bZ*valor.cY} + ${valor.aZ*valor.bY*valor.cX})`;
+    D3.innerHTML = `D= ${(valor.aX*valor.bY*valor.cZ)+(valor.aY*valor.bZ*valor.cX)+(valor.aZ*valor.bX*valor.cY)} - ${(valor.aY*valor.bX*valor.cZ) + (valor.aX*valor.bZ*valor.cY) + (valor.aZ*valor.bY*valor.cX)}`;
+    D.innerHTML = `D= ${((valor.aX*valor.bY*valor.cZ)+(valor.aY*valor.bZ*valor.cX)+(valor.aZ*valor.bX*valor.cY)) - ((valor.aY*valor.bX*valor.cZ) + (valor.aX*valor.bZ*valor.cY) + (valor.aZ*valor.bY*valor.cX))}`;
+}
+
+function Calculo_DX(){
+    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    let DX1 = document.getElementById('DX1');
+    let DX2 = document.getElementById('DX2');
+    let DX3 = document.getElementById('DX3');
+    let DX = document.getElementById('DX');
+    DX1.innerHTML = `Dx= ((${valor.aN} * ${valor.bY} * ${valor.cZ}) + (${valor.aY} * ${valor.bZ} * ${valor.cN}) + (${valor.aZ} * ${valor.bN} * ${valor.cY})) - ((${valor.aY} * ${valor.bN} * ${valor.cZ}) + (${valor.aN} * ${valor.bZ}* ${valor.cY}) + (${valor.aZ} * ${valor.bY} * ${valor.cN}))`;
+    DX2.innerHTML = `Dx= (${valor.aN*valor.bY*valor.cZ} + ${valor.aY*valor.bZ*valor.cN} + ${valor.aZ*valor.bN*valor.cY}) - (${valor.aY*valor.bN*valor.cZ} + ${valor.aN*valor.bZ*valor.cY} + ${valor.aZ*valor.bY*valor.cN})`;
+    DX3.innerHTML = `Dx= ${(valor.aN*valor.bY*valor.cZ)+(valor.aY*valor.bZ*valor.cN)+(valor.aZ*valor.bN*valor.cY)} - ${(valor.aY*valor.bN*valor.cZ) + (valor.aN*valor.bZ*valor.cY) + (valor.aZ*valor.bY*valor.cN)}`;
+    DX.innerHTML = `Dx= ${((valor.aN*valor.bY*valor.cZ)+(valor.aY*valor.bZ*valor.cN)+(valor.aZ*valor.bN*valor.cY)) - ((valor.aY*valor.bN*valor.cZ) + (valor.aN*valor.bZ*valor.cY) + (valor.aZ*valor.bY*valor.cN))}`;
+}
+
+function Calculo_DY(){
+    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    let DY1 = document.getElementById('DY1');
+    let DY2 = document.getElementById('DY2');
+    let DY3 = document.getElementById('DY3');
+    let DY = document.getElementById('DY');
+    DY1.innerHTML = `Dy= ((${valor.aX} * ${valor.bN} * ${valor.cZ}) + (${valor.aN} * ${valor.bZ} * ${valor.cX}) + (${valor.aZ} * ${valor.bX} * ${valor.cN})) - ((${valor.aN} * ${valor.bX} * ${valor.cZ}) + (${valor.aX} * ${valor.bZ}* ${valor.cN}) + (${valor.aZ} * ${valor.bN} * ${valor.cX}))`;
+    DY2.innerHTML = `Dy=  (${valor.aX*valor.bN*valor.cZ} + ${valor.aN*valor.bZ*valor.cX} + ${valor.aZ*valor.bX*valor.cN}) - (${valor.aN*valor.bX*valor.cZ} + ${valor.aX*valor.bZ*valor.cN} + ${valor.aZ*valor.bN*valor.cX})`;
+    DY3.innerHTML = `Dy=  ${(valor.aX*valor.bN*valor.cZ)+(valor.aN*valor.bZ*valor.cX)+(valor.aZ*valor.bX*valor.cN)} - ${(valor.aN*valor.bX*valor.cZ) + (valor.aX*valor.bZ*valor.cN) + (valor.aZ*valor.bN*valor.cX)}`;
+    DY.innerHTML = `Dy=  ${((valor.aX*valor.bN*valor.cZ)+(valor.aN*valor.bZ*valor.cX)+(valor.aZ*valor.bX*valor.cN)) - ((valor.aN*valor.bX*valor.cZ) + (valor.aX*valor.bZ*valor.cN) + (valor.aZ*valor.bN*valor.cX))}`;
+}
+
+function Calculo_DZ(){
+    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    let DZ1 = document.getElementById('DZ1');
+    let DZ2 = document.getElementById('DZ2');
+    let DZ3 = document.getElementById('DZ3');
+    let DZ = document.getElementById('DZ');
+
+    DZ1.innerHTML = `Dz= ((${valor.aX} * ${valor.bY} * ${valor.cN}) + (${valor.aY} * ${valor.bN} * ${valor.cX}) + (${valor.aN} * ${valor.bX} * ${valor.cY})) - ((${valor.aY} * ${valor.bX} * ${valor.cN}) + (${valor.aX} * ${valor.bN}* ${valor.cY}) + (${valor.aN} * ${valor.bY} * ${valor.cX}))`;
+    DZ2.innerHTML = `Dz= (${valor.aX*valor.bY*valor.cN} + ${valor.aY*valor.bN*valor.cX} + ${valor.aN*valor.bX*valor.cY}) - (${valor.aY*valor.bX*valor.cN} + ${valor.aX*valor.bN*valor.cY} + ${valor.aN*valor.bY*valor.cX})`;
+    DZ3.innerHTML = `Dz= ${(valor.aX*valor.bY*valor.cN)+(valor.aY*valor.bN*valor.cX)+(valor.aN*valor.bX*valor.cY)} - ${(valor.aY*valor.bX*valor.cN) + (valor.aX*valor.bN*valor.cY) + (valor.aN*valor.bY*valor.cX)}`;
+    DZ.innerHTML = `Dz= ${((valor.aX*valor.bY*valor.cN)+(valor.aY*valor.bN*valor.cX)+(valor.aN*valor.bX*valor.cY)) - ((valor.aY*valor.bX*valor.cN) + (valor.aX*valor.bN*valor.cY) + (valor.aN*valor.bY*valor.cX))}`;
+}
+
+function Chamar() {
+    let resultado = document.getElementById('equacao');
+    resultado.innerHTML = "";
 
     var valor = JSON.parse(sessionStorage.getItem('meusValores'));
 
-    for(var i in valor){
-        let p = document.createElement('p');
-        p.innerHTML = valor[i];
-        resultado.append(p);
+    resultado.innerHTML = `${valor.aX}x + ${valor.aY}y + ${valor.aZ}z = ${valor.aN}<br>${valor.bX}x + ${valor.bY}y + ${valor.bZ}z = ${valor.bN}<br>${valor.cX}x + ${valor.cY}y + ${valor.cZ}z = ${valor.cN}`;
+    
+    
+    let aX = document.getElementsByName('aX');
+    let bX = document.getElementsByClassName('bX');
+    let cX = document.getElementsByClassName('cX');
+    let aY = document.getElementsByClassName('aY');
+    let bY = document.getElementsByClassName('bY');
+    let cY = document.getElementsByClassName('cY');
+    let aZ = document.getElementsByClassName('aZ');
+    let bZ = document.getElementsByClassName('bZ');
+    let cZ = document.getElementsByClassName('cZ');
+    let aN = document.getElementsByClassName('aN');
+    let bN = document.getElementsByClassName('bN');
+    let cN = document.getElementsByClassName('cN');
+
+    for (var i = 0; i < aX.length; i++) {
+        aX[i].innerHTML = valor.aX;
+        bX[i].innerHTML = valor.bX;
+        cX[i].innerHTML = valor.cX;
+        aY[i].innerHTML = valor.aY;
+        bY[i].innerHTML = valor.bY;
+        cY[i].innerHTML = valor.cY;
     }
+
+    for (var i = 0; i < aZ.length; i++) {
+        aZ[i].innerHTML = valor.aZ;
+        bZ[i].innerHTML = valor.bZ;
+        cZ[i].innerHTML = valor.cZ;
+    }
+    for (var i = 0; i < aN.length; i++) {
+        aN[i].innerHTML = valor.aN;
+        bN[i].innerHTML = valor.bN;
+        cN[i].innerHTML = valor.cN;
+    }
+
 }
 
 
 window.onload = function () {
 
+    Chamar();
+    Calculo_D();
+    Calculo_DX();
+    Calculo_DY();
+    Calculo_DZ();
+
     alert("função funcionando");
 
-    
     (async function () {
         while (true) {
             const celulas = [];
@@ -74,4 +160,6 @@ window.onload = function () {
             }
         }
     })();
+
+    
 }
--- Iniciação Cientifica/Tela_Inicial/script.js
+++ Iniciação Cientifica/Tela_Inicial/script.js
@@ -6,27 +6,27 @@ const Calcular = () => {
         alert("não há valor a ser retornado");
     }
 
-    var a1 = document.getElementById('aX').value;
-    var b1 = document.getElementById('bY').value;
-    var c1 = document.getElementById('cZ').value;
-    var n1 = document.getElementById('dN').value;
-    var a2 = document.getElementById('eX').value;
-    var b2 = document.getElementById('fY').value;
-    var c2 = document.getElementById('gZ').value;
-    var n2 = document.getElementById('hN').value;
-    var a3 = document.getElementById('iX').value;
-    var b3 = document.getElementById('jY').value;
-    var c3 = document.getElementById('kZ').value;
-    var n3 = document.getElementById('lN').value;
+    var aX = document.getElementById('aX').value;
+    var aY = document.getElementById('bY').value;
+    var aZ = document.getElementById('cZ').value;
+    var aN = document.getElementById('dN').value;
+    var bX = document.getElementById('eX').value;
+    var bY = document.getElementById('fY').value;
+    var bZ = document.getElementById('gZ').value;
+    var bN = document.getElementById('hN').value;
+    var cX = document.getElementById('iX').value;
+    var cY = document.getElementById('jY').value;
+    var cZ = document.getElementById('kZ').value;
+    var cN = document.getElementById('lN').value;
 
-    valores = {a1: a1, a2: a2, a3: a3, b1: b1, b2: b2, b3: b3, c1: c1, c2: c2, c3: c3, n1: n1, n2: n2, n3: n3};
+    valores = {aX: aX, bX: bX, cX: cX, aY: aY, bY: bY, cY: cY, aZ: aZ, bZ: bZ, cZ: cZ, aN: aN, bN: bN, cN: cN};
 
     sessionStorage.meusValores = JSON.stringify(valores);
 
-    // const dd = ((a1 * b2 * c3) + (b1 * c2 * a3) + (c1 * a2 * b3)) - ((b1 * a2 * c3) + (a1 * c2 * b3) + (c1 * b2 * a3));
-    // const x1 = (((n1 * b2 * c3) + (b1 * c2 * n3) + (c1 * n2 * b3)) - ((b1 * n2 * c3) + (n1 * c2 * b3) + (c1 * b2 * n3))) / dd;
-    // const x2 = (((a1 * n2 * c3) + (n1 * c2 * a3) + (c1 * a2 * n3)) - ((n1 * a2 * c3) + (a1 * c2 * n3) + (c1 * n2 * a3))) / dd;
-    // const x3 = (((a1 * b2 * n3) + (b1 * n2 * a3) + (n1 * a2 * b3)) - ((b1 * a2 * n3) + (a1 * n2 * b3) + (n1 * b2 * a3))) / dd;
+    // const dd = ((aX * bY * cZ) + (aY * bZ * cX) + (aZ * bX * cY)) - ((aY * bX * cZ) + (aX * bZ * cY) + (aZ * bY * cX));
+    // const x1 = (((aN * bY * cZ) + (aY * bZ * cN) + (aZ * bN * cY)) - ((aY * bN * cZ) + (aN * bZ * cY) + (aZ * bY * cN))) / dd;
+    // const x2 = (((aX * bN * cZ) + (aN * bZ * cX) + (aZ * bX * cN)) - ((aN * bX * cZ) + (aX * bZ * cN) + (aZ * bN * cX))) / dd;
+    // const x3 = (((aX * bY * cN) + (aY * bN * cX) + (aN * bX * cY)) - ((aY * bX * cN) + (aX * bN * cY) + (aN * bY * cX))) / dd;
 
     // // window.location.href = 'processo.html'
 
