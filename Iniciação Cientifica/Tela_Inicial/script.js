ea7635e480bef1b87ad8d7419d2468d42e5eba6f
AliePonzani <115513954+AliePonzani@users.noreply.github.com>
2023-05-12 15:08:05 +0000

    alterado

--- Iniciação Cientifica/Tela_Inicial/script.js
+++ Iniciação Cientifica/Tela_Inicial/script.js
@@ -29,22 +29,27 @@ const Calcular = () => {
         else {
             const cliqueAqui = document.querySelector('#cliqueAqui');
             cliqueAqui.href = "../Segunda_Tela/segundaTela.html";
+            sessionStorage.meusValores = JSON.stringify(valores);
+
+            const dd = ((aX * bY * cZ) + (aY * bZ * cX) + (aZ * bX * cY)) - ((aY * bX * cZ) + (aX * bZ * cY) + (aZ * bY * cX));
+
+            if (dd == 0) {
+                alert("teste");
+            } else {
+                const x1 = (((aN * bY * cZ) + (aY * bZ * cN) + (aZ * bN * cY)) - ((aY * bN * cZ) + (aN * bZ * cY) + (aZ * bY * cN))) / dd;
+                const x2 = (((aX * bN * cZ) + (aN * bZ * cX) + (aZ * bX * cN)) - ((aN * bX * cZ) + (aX * bZ * cN) + (aZ * bN * cX))) / dd;
+                const x3 = (((aX * bY * cN) + (aY * bN * cX) + (aN * bX * cY)) - ((aY * bX * cN) + (aX * bN * cY) + (aN * bY * cX))) / dd;
+
+                // // window.location.href = 'processo.html'
+
+                let exibirResultado = document.getElementById('resultado');
+
+                exibirResultado.innerHTML = `x = ${x1.toFixed()} <br> y = ${x2.toFixed()} <br> z = ${x3.toFixed()} <br>`;
+                exibirResultado.style.visibility = "visible";
+            }
         }
     }
 
-    sessionStorage.meusValores = JSON.stringify(valores);
-
-    // const dd = ((aX * bY * cZ) + (aY * bZ * cX) + (aZ * bX * cY)) - ((aY * bX * cZ) + (aX * bZ * cY) + (aZ * bY * cX));
-    // const x1 = (((aN * bY * cZ) + (aY * bZ * cN) + (aZ * bN * cY)) - ((aY * bN * cZ) + (aN * bZ * cY) + (aZ * bY * cN))) / dd;
-    // const x2 = (((aX * bN * cZ) + (aN * bZ * cX) + (aZ * bX * cN)) - ((aN * bX * cZ) + (aX * bZ * cN) + (aZ * bN * cX))) / dd;
-    // const x3 = (((aX * bY * cN) + (aY * bN * cX) + (aN * bX * cY)) - ((aY * bX * cN) + (aX * bN * cY) + (aN * bY * cX))) / dd;
-
-    // // window.location.href = 'processo.html'
-
-    // let exibirResultado = document.getElementById('resultado');
-
-    // exibirResultado.innerHTML = `x = ${x1.toFixed()} <br> y = ${x2.toFixed()} <br> z = ${x3.toFixed()} <br>`
-    // exibirResultado.style.visibility = "visible";
 
 
 }
