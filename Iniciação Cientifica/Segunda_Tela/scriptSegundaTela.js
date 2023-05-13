f8d7832c234dbe2bbaada53e6c4229b57965578d
AliePonzani <115513954+AliePonzani@users.noreply.github.com>
2023-05-13 00:33:38 +0000

    finalizado!

--- Iniciação Cientifica/Segunda_Tela/scriptSegundaTela.js
+++ Iniciação Cientifica/Segunda_Tela/scriptSegundaTela.js
@@ -1,68 +1,73 @@
+var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+var Determinante = ((valor.aX * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cY)) - ((valor.aY * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cX));
+
 function Calculo_D() {
-    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    
     let D1 = document.getElementById('D1');
     let D2 = document.getElementById('D2');
     let D3 = document.getElementById('D3');
     let D = document.getElementById('D');
+    
 
     D1.innerHTML = `D= ((${valor.aX} * ${valor.bY} * ${valor.cZ}) + (${valor.aY} * ${valor.bZ} * ${valor.cX}) + (${valor.aZ} * ${valor.bX} * ${valor.cY})) - ((${valor.aY} * ${valor.bX} * ${valor.cZ}) + (${valor.aX} * ${valor.bZ}* ${valor.cY}) + (${valor.aZ} * ${valor.bY} * ${valor.cX}))`;
     D2.innerHTML = `D= (${valor.aX * valor.bY * valor.cZ} + ${valor.aY * valor.bZ * valor.cX} + ${valor.aZ * valor.bX * valor.cY}) - (${valor.aY * valor.bX * valor.cZ} + ${valor.aX * valor.bZ * valor.cY} + ${valor.aZ * valor.bY * valor.cX})`;
     D3.innerHTML = `D= ${(valor.aX * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cY)} - ${(valor.aY * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cX)}`;
-    D.innerHTML = `D= ${((valor.aX * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cY)) - ((valor.aY * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cX))}`;
+    D.innerHTML = `D= ${Determinante}`;
 }
 
 function Calculo_DX() {
-    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    
     let DX1 = document.getElementById('DX1');
     let DX2 = document.getElementById('DX2');
     let DX3 = document.getElementById('DX3');
     let DX = document.getElementById('DX');
+    let valorDeX = document.getElementById('valorDeX');
+    var resultado = ((valor.aN * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cY)) - ((valor.aY * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cN));
     DX1.innerHTML = `Dx= ((${valor.aN} * ${valor.bY} * ${valor.cZ}) + (${valor.aY} * ${valor.bZ} * ${valor.cN}) + (${valor.aZ} * ${valor.bN} * ${valor.cY})) - ((${valor.aY} * ${valor.bN} * ${valor.cZ}) + (${valor.aN} * ${valor.bZ}* ${valor.cY}) + (${valor.aZ} * ${valor.bY} * ${valor.cN}))`;
     DX2.innerHTML = `Dx= (${valor.aN * valor.bY * valor.cZ} + ${valor.aY * valor.bZ * valor.cN} + ${valor.aZ * valor.bN * valor.cY}) - (${valor.aY * valor.bN * valor.cZ} + ${valor.aN * valor.bZ * valor.cY} + ${valor.aZ * valor.bY * valor.cN})`;
     DX3.innerHTML = `Dx= ${(valor.aN * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cY)} - ${(valor.aY * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cN)}`;
-    DX.innerHTML = `Dx= ${((valor.aN * valor.bY * valor.cZ) + (valor.aY * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cY)) - ((valor.aY * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cY) + (valor.aZ * valor.bY * valor.cN))}`;
+    DX.innerHTML = `Dx= ${resultado}`;
+    valorDeX.innerHTML = `${resultado}/${Determinante} então x = ${resultado/Determinante}`;
 }
 
 function Calculo_DY() {
-    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    
     let DY1 = document.getElementById('DY1');
     let DY2 = document.getElementById('DY2');
     let DY3 = document.getElementById('DY3');
     let DY = document.getElementById('DY');
+    let valorDeY = document.getElementById('valorDeY');
+    var resultado = ((valor.aX * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cN)) - ((valor.aN * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cX));
     DY1.innerHTML = `Dy= ((${valor.aX} * ${valor.bN} * ${valor.cZ}) + (${valor.aN} * ${valor.bZ} * ${valor.cX}) + (${valor.aZ} * ${valor.bX} * ${valor.cN})) - ((${valor.aN} * ${valor.bX} * ${valor.cZ}) + (${valor.aX} * ${valor.bZ}* ${valor.cN}) + (${valor.aZ} * ${valor.bN} * ${valor.cX}))`;
     DY2.innerHTML = `Dy=  (${valor.aX * valor.bN * valor.cZ} + ${valor.aN * valor.bZ * valor.cX} + ${valor.aZ * valor.bX * valor.cN}) - (${valor.aN * valor.bX * valor.cZ} + ${valor.aX * valor.bZ * valor.cN} + ${valor.aZ * valor.bN * valor.cX})`;
     DY3.innerHTML = `Dy=  ${(valor.aX * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cN)} - ${(valor.aN * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cX)}`;
-    DY.innerHTML = `Dy=  ${((valor.aX * valor.bN * valor.cZ) + (valor.aN * valor.bZ * valor.cX) + (valor.aZ * valor.bX * valor.cN)) - ((valor.aN * valor.bX * valor.cZ) + (valor.aX * valor.bZ * valor.cN) + (valor.aZ * valor.bN * valor.cX))}`;
+    DY.innerHTML = `Dy=  ${resultado/Determinante}`;
+    valorDeY.innerHTML = `${resultado}/${Determinante} então y = ${resultado/Determinante}`;
+
 }
 
 function Calculo_DZ() {
-    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    
     let DZ1 = document.getElementById('DZ1');
     let DZ2 = document.getElementById('DZ2');
     let DZ3 = document.getElementById('DZ3');
     let DZ = document.getElementById('DZ');
-
+    let valorDeZ = document.getElementById('valorDeZ');
+    var resultado = ((valor.aX * valor.bY * valor.cN) + (valor.aY * valor.bN * valor.cX) + (valor.aN * valor.bX * valor.cY)) - ((valor.aY * valor.bX * valor.cN) + (valor.aX * valor.bN * valor.cY) + (valor.aN * valor.bY * valor.cX));
     DZ1.innerHTML = `Dz= ((${valor.aX} * ${valor.bY} * ${valor.cN}) + (${valor.aY} * ${valor.bN} * ${valor.cX}) + (${valor.aN} * ${valor.bX} * ${valor.cY})) - ((${valor.aY} * ${valor.bX} * ${valor.cN}) + (${valor.aX} * ${valor.bN}* ${valor.cY}) + (${valor.aN} * ${valor.bY} * ${valor.cX}))`;
     DZ2.innerHTML = `Dz= (${valor.aX * valor.bY * valor.cN} + ${valor.aY * valor.bN * valor.cX} + ${valor.aN * valor.bX * valor.cY}) - (${valor.aY * valor.bX * valor.cN} + ${valor.aX * valor.bN * valor.cY} + ${valor.aN * valor.bY * valor.cX})`;
     DZ3.innerHTML = `Dz= ${(valor.aX * valor.bY * valor.cN) + (valor.aY * valor.bN * valor.cX) + (valor.aN * valor.bX * valor.cY)} - ${(valor.aY * valor.bX * valor.cN) + (valor.aX * valor.bN * valor.cY) + (valor.aN * valor.bY * valor.cX)}`;
-    DZ.innerHTML = `Dz= ${((valor.aX * valor.bY * valor.cN) + (valor.aY * valor.bN * valor.cX) + (valor.aN * valor.bX * valor.cY)) - ((valor.aY * valor.bX * valor.cN) + (valor.aX * valor.bN * valor.cY) + (valor.aN * valor.bY * valor.cX))}`;
+    DZ.innerHTML = `Dz= ${resultado}`;
+    valorDeZ.innerHTML = `${resultado}/${Determinante} então z = ${resultado/Determinante}`;
 }
 
 function Chamar() {
     let resultado = document.getElementById('equacao');
     resultado.innerHTML = "";
 
-    var valor = JSON.parse(sessionStorage.getItem('meusValores'));
+    
 
-    for(var j in valor){
-        if(!valor[j]){
-            alert("Todos os campos da tela anterior devem ser preenchidos!");
-            history.back();
-            break;
-        }
-    }
-
-    resultado.innerHTML = `${valor.aX}x + ${valor.aY}y + ${valor.aZ}z = ${valor.aN}<br>${valor.bX}x + ${valor.bY}y + ${valor.bZ}z = ${valor.bN}<br>${valor.cX}x + ${valor.cY}y + ${valor.cZ}z = ${valor.cN}`;
+    resultado.innerHTML = `<p>${valor.aX}x + ${valor.aY}y + ${valor.aZ}z = ${valor.aN}<br>${valor.bX}x + ${valor.bY}y + ${valor.bZ}z = ${valor.bN}<br>${valor.cX}x + ${valor.cY}y + ${valor.cZ}z = ${valor.cN}</p>`;
 
 
     let aX = document.getElementsByClassName('aX');
